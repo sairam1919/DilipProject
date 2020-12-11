7d16d314aa9d4d2ff19caa203de16db8f3d9ffae
psairamr <ponnala.sairamreddy@external.merckgroup.com>
2020-12-11 12:52:01 -0600

    Tickets API's udpated and API info file also addedd

--- server/routes/ticket.js
+++ server/routes/ticket.js
@@ -14,6 +14,7 @@ let apiResponse = { "message": "", "result": "", "statuscode": "" };
 
 //GET API to Fetch all Users
 router.get("/", (req, res) => {
+    console.log("Request Received");
     connection.query('SELECT * FROM tickets', function (error, results, fields) {
         if (error) {
             apiResponse.message = "Error while connecting database"
@@ -36,8 +37,8 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/getTicketsbyUserName/:id", (req, res) => {
-    let query1 = 'SELECT * FROM tickets WHERE user_name = ' + req.params.id ;
+router.get("/getTicketbyTicketID/:id", (req, res) => {
+    let query1 = 'SELECT * FROM tickets WHERE ticket_id = ' + "'" + req.params.id + "'";
     connection.query(query1, function (error, results, fields) {
         if (error) {
             apiResponse.message = "Error while connecting database"
@@ -60,8 +61,32 @@ router.get("/getTicketsbyUserName/:id", (req, res) => {
     });
 });
 
-router.get("/getTicketsbyAssignedName/:id", (req, res) => {
-    let query1 = 'SELECT * FROM tickets WHERE ticket_assignedto = ' + req.params.id ;
+router.get("/getTicketsbyUserName/:id", (req, res) => {
+    let query1 = 'SELECT * FROM tickets WHERE user_name = ' + "'" + req.params.id + "'";
+    connection.query(query1, function (error, results, fields) {
+        if (error) {
+            apiResponse.message = "Error while connecting database"
+            apiResponse.statuscode = "400";
+            apiResponse.result = error;
+            res.status(400).send(apiResponse);
+        } else {
+            if (results.length > 0) {
+                apiResponse.statuscode = "200";
+                apiResponse.message = "Successfully Fetched the Available Tickets";
+                apiResponse.result = results;
+                res.status(200).send(apiResponse);
+            } else {
+                apiResponse.statuscode = "2001";
+                apiResponse.message = "No Tickets Available";
+                apiResponse.result = results;
+                res.status(200).send(apiResponse);
+            }
+        }
+    });
+});
+
+router.get("/getTicketsbyAssigneeName/:id", (req, res) => {
+    let query1 = 'SELECT * FROM tickets WHERE ticket_assignedto = ' + "'" + req.params.id + "'";
     connection.query(query1, function (error, results, fields) {
         if (error) {
             apiResponse.message = "Error while connecting database"
@@ -98,8 +123,9 @@ router.post("/", (req, res) => {
     let ticket_status = req.body.ticket_status;
     let ticket_resolution = [];
     let ticket_assignedto = '';
+    let ticket_createdby = req.body.ticket_createdby;
 
-    var query1 = "INSERT INTO tickets ( user_name, email, ticket_id, ticket_content, ticket_area, ticket_attachments, comments, ticket_createddate, ticket_updateddate,ticket_status, ticket_resolution, ticket_assignedto ) VALUES  ( " + "'" + user_name + "'" + " , " + "'" + email + "'" + " , " + "'" + ticket_id + "'" + " , " + "'" + ticket_content + "'" + " , " + "'" + ticket_area + "'" + " , " + "'" + ticket_attachments + "'" + " , " + "'" + comments + "'" + " , " + "'" + ticket_createddate + "'" + " , " + "'" + ticket_updateddate + "'" + " , " + "'" + ticket_status + "'" + " , " + "'" + ticket_resolution + "'" + " , " + "'" + ticket_assignedto + "'" + " )";
+    var query1 = "INSERT INTO tickets ( user_name, email, ticket_id, ticket_content, ticket_area, ticket_attachments, comments, ticket_createddate, ticket_updateddate,ticket_status, ticket_resolution, ticket_assignedto, ticket_createdby ) VALUES  ( " + "'" + user_name + "'" + " , " + "'" + email + "'" + " , " + "'" + ticket_id + "'" + " , " + "'" + ticket_content + "'" + " , " + "'" + ticket_area + "'" + " , " + "'" + ticket_attachments + "'" + " , " + "'" + comments + "'" + " , " + "'" + ticket_createddate + "'" + " , " + "'" + ticket_updateddate + "'" + " , " + "'" + ticket_status + "'" + " , " + "'" + ticket_resolution + "'" + " , " + "'" + ticket_assignedto + "'" + " , " + "'" + ticket_createdby + "'" + " )";
     connection.query(query1, function (error, results, fields) {
         if (error) {
             apiResponse.message = "Error while connecting database"
@@ -108,7 +134,7 @@ router.post("/", (req, res) => {
             res.status(400).send(apiResponse);
         } else {
             apiResponse.statuscode = "200";
-            apiResponse.message = "User Created Successfully";
+            apiResponse.message = "Ticket Created Successfully";
             apiResponse.result = {};
             res.status(200).send(apiResponse);
         }
@@ -117,12 +143,12 @@ router.post("/", (req, res) => {
 
 router.put("/:id", (req, res) => {
     let ticket_attachments = req.body.ticket_attachments;
-    let comments = [];
+    let comments = req.body.comments;
     let ticket_updateddate = req.body.ticket_updateddate;
     let ticket_status = req.body.ticket_status;
-    let ticket_resolution = [];
-    let ticket_assignedto = '';
-    var query1 = "UPDATE tickets SET ticket_attachments= " + ticket_attachments + " , comments=  " + comments + " , ticket_updateddate=  " + ticket_updateddate + " , ticket_status=  " + ticket_status + " , ticket_resolution=  " + ticket_resolution + " , ticket_assignedto=  " + ticket_assignedto + " WHERE ticket_id= " + req.params.id;
+    let ticket_resolution = req.body.ticket_resolution;
+    let ticket_assignedto = req.body.ticket_assignedto;
+    var query1 = "UPDATE tickets SET ticket_attachments= " + "'" + ticket_attachments + "'" + " , comments=  " + "'" + comments + "'" + " , ticket_updateddate=  " + "'" + ticket_updateddate + "'" + " , ticket_status=  " + "'" + ticket_status + "'" + " , ticket_resolution=  " + "'" + ticket_resolution + "'" + " , ticket_assignedto=  " + "'" + ticket_assignedto + "'" + " WHERE ticket_id= " + "'" + req.params.id + "'";
     connection.query(query1, function (error, results, fields) {
         if (error) {
             apiResponse.message = "Error while connecting database"
@@ -132,7 +158,7 @@ router.put("/:id", (req, res) => {
         } else {
             apiResponse.statuscode = "200";
             apiResponse.message = "Ticket Updated Successfully";
-            apiResponse.result = results;
+            apiResponse.result = {};
             res.status(200).send(apiResponse);
         }
     });
